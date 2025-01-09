7d802e60d54353994adf91d911c973968f7eaa54
fahim <fahimahamedweb@gmail.com>
2025-01-10 04:02:59 +0600

    deploy vercel

--- bestbuyproject/settings.py
+++ bestbuyproject/settings.py
@@ -115,11 +115,18 @@ DATABASES = {
      'default': dj_database_url.config(
          default=config('DATABASE_URL')
      )
- }"""
+ }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': str(BASE_DIR / 'db.sqlite3'),
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # Password validation
