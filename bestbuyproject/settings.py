1a73f61e96c2fc814a7c581f094adf47f59f216a
fahim <fahimahamedweb@gmail.com>
2025-01-10 04:02:24 +0600

    deploy vercel

--- bestbuyproject/settings.py
+++ bestbuyproject/settings.py
@@ -169,8 +169,8 @@ STATICFILES_DIRS = [
 #print(STATIC_DIR)
 STATIC_URL = '/static/'
 #media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
