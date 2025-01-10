bf6ad934dbab2a681d139012b7218b3f87f4cefc
fahim <fahimahamedweb@gmail.com>
2025-01-11 00:09:34 +0600

    fixed

--- bestbuyproject/settings.py
+++ bestbuyproject/settings.py
@@ -177,8 +177,8 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 #media files
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
