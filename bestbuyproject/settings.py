d7812de75041f152a1f2ace1dfd0f31c1bdbd0f0
fahim <fahimahamedweb@gmail.com>
2025-01-12 12:11:43 +0600

    deploy

--- bestbuyproject/settings.py
+++ bestbuyproject/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = 'f^=5pd1lso=1zxm*da!r$=@%o937%zv+4n7pvtgkoiuk3tkps('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','.now.sh']
 
 # Application definition
 
