56dcecd014f888e97185dbf07c9a07ad1f20dd4e
fahim <fahimahamedweb@gmail.com>
2025-01-12 12:10:09 +0600

    deploy

--- bestbuyproject/settings.py
+++ bestbuyproject/settings.py
@@ -124,11 +124,10 @@ DATABASES = {
 }"""
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': dj_database_url.config(
+         default=config('DATABASE_URL')
+     )
+ }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
